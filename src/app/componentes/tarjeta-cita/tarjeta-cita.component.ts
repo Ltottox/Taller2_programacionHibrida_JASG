c78f5efcaf7004101173e81bb402045fdd1ce5cf
Jose Saldivia <antoniogb.saldivia@gmail.com>
2026-01-05 13:03:01 -0300

    html de tarjeta cita y ts

--- src/app/componentes/tarjeta-cita/tarjeta-cita.component.ts
+++ src/app/componentes/tarjeta-cita/tarjeta-cita.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-tarjeta-cita',
   templateUrl: './tarjeta-cita.component.html',
   styleUrls: ['./tarjeta-cita.component.scss'],
   standalone: true,
+  imports: [IonCardContent, IonCardTitle, IonCardHeader, IonCard],
 })
 export class TarjetaCitaComponent  implements OnInit {
+cita: any;
 
   constructor() { }
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { TarjetaCitaComponent } from "../componentes/tarjeta-cita/tarjeta-cita.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, TarjetaCitaComponent],
 })
 export class HomePage {
   constructor() {}
